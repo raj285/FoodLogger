e1f0df2d888ffcb786e20f50c30b449e9730770b
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-18 11:53:34 +0530

    database connected

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { main } = require("./database.js");
+const { main } = require("./config/database.js");
+const {User}=require('./model/User.js');
 main();
 const app = express();
 dotenv.config();
-
+app.use(express.json());
+app.post('/signup',async (req,res)=>{
+  try {
+    console.log("mai kaam kr rha hun bc")
+    let data=new User(req.body);
+    console.log(data);
+    await data.save();
+    res.send("signed up successfully");
+  } catch (error) {
+    res.send(`something went wrong : ${error}`);
+  }
+})
+app.get('/login', async(req,res)=>{
+  try {
+    let data=req.body;
+    
+  } catch (error) {
+    
+  }
+})
 let PORT = Number(process.env.PORT) || 8080;
 app.listen(PORT, () => {
   try {
--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ const main = async () => {
     await mongoose.connect(
       "mongodb+srv://goswamiraj2520:SRIpJ9pYficcpUYn@namastenode.wwp0s.mongodb.net/FoodLogger"
     );
+    console.log("Database connected successfully!");
   } catch (error) {
     console.error("something went wrong "+error);
   }
