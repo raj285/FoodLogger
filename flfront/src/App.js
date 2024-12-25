0ce1e526262186be4cb30231d9dc1e05044c545a
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-25 17:14:08 +0530

    mn

--- flfront/src/App.js
+++ flfront/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import './components/Sidebar';
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <>
-      <h1>कर्म सर्वमहत्त्वम् अस्ति।</h1>
+      <h1 className='bg-emerald-700 text-red-600'> कर्म सर्वमहत्त्वम् अस्ति।</h1>
       <h1>food log</h1>
-      
+      <Header/>
+      <Sidebar/>
+      <MainContent/>
+      <Footer/>
     </>
   );
 }
