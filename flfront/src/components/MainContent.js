f18c53f2d3098e7682b92edb55a0e868236ee127
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-29 16:43:29 +0530

    was checking how to get the redux data .......and BINGO we did it

--- flfront/src/components/MainContent.js
+++ flfront/src/components/MainContent.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+
 function MainContent() {
+  // Use useSelector to directly access the Redux state
+  const token = useSelector((state) => state.auth.token); // Access token from the auth slice
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); // Access authentication status
+
   return (
-<>iahuysgdshuaijshdgvdsa</>
+    <>
+      <p>Token Information:</p>
+      {isAuthenticated ? (
+        <h1>Your Token: {token}</h1>
+      ) : (
+        <h1>You are not authenticated. Please log in.</h1>
+      )}
+    </>
   );
 }
 
-export default MainContent
+export default MainContent;
